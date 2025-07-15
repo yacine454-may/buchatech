7ff9e4582ecdca3211aa62479dcab5be2cbb5127
Mehdi Ahmed Yacine <yacinemehdi2005@gmail.com>
2025-07-15 20:49:50 +0100

    Update main.tsx

--- bucha-tech/src/main.tsx
+++ bucha-tech/src/main.tsx
@@ -21,6 +21,9 @@ const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
   return <>{children}</>;
 };
 
+// Ajouté tout en haut du fichier pour debug Vercel
+console.log('API_URL:', (import.meta as any).env.VITE_API_URL);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider>
